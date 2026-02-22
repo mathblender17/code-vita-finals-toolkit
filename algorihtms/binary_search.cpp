c6bd67902f0fff2ad6704f21abb39bfa67677ab0
Meghraj Mahesh Patil <114827353+mathblender17@users.noreply.github.com>
2026-02-22 15:23:54 +0530

    Create binary_search.cpp

---
+++ algorihtms/binary_search.cpp
@@ -0,0 +1,21 @@
+int binarySearch(vector<int>& arr, int target) {
+
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while(left <= right) {
+
+        int mid = left + (right-left)/2;
+
+        if(arr[mid] == target)
+            return mid;
+
+        else if(arr[mid] < target)
+            left = mid + 1;
+
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
