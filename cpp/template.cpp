53e73de4a94d9eaf36d03215b901d57387eaddb2
Meghraj Mahesh Patil <114827353+mathblender17@users.noreply.github.com>
2026-02-22 11:25:18 +0530

    Add master template for coding problems

---
+++ cpp/template.cpp
@@ -0,0 +1,32 @@
+// Master Template (USE THIS FOR EVERY PROBLEM)
+
+// Source: Meghraj Patil Code Vita Finals Toolkit
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#define endl '\n'
+
+void solve() {
+
+}
+
+int main() {
+
+    fastio();
+
+    int t = 1;
+    // Uncomment if multiple test cases
+    // cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
